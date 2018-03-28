3010b364249fa45ac2850df451faf5a73ea30759
Jason Jung <hyeonsuns@naver.com>
2018-03-28 18:15:16 +0900

    Daily coding Divide and Counquer_2

--- Divide_Conquer/Divide_Conquer/1780_종이의개수.cpp
+++ Divide_Conquer/Divide_Conquer/1780_종이의개수.cpp
@@ -2,27 +2,47 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+int n;
+int a[2188][2188];
+int result[3]; // -1, 0, 1
 
-int a[2187][2187];
-int n, m, p;
+int checkSameNum(int startRow, int endRow, int startCol, int endCol);
+void solve(int startRow, int endRow, int startCol, int endCol);
 
-int ternary_search(int num) {
-	int left = 0;
-	int right = n - 1;
-	while (left <= right) {
-		int first = (left + right) / 3;
-		int second = (left + right) * (2 / 3);
-		if (a)
-	}
-}
 int main(void)
 {
-	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			cin >> a[i][j];
 
-	int zeroCount = 0, oneCount = 0, minusCount = 0;
+	solve(0, n - 1, 0, n - 1);
 
+	for (int i = 0; i < 3; i++)
+		cout << result[i] << ' ';
+	return 0;
+}
+int checkSameNum(int startRow, int endRow, int startCol, int endCol)
+{
+	int temp = a[startRow][startCol];
+	for (int i = startRow; i <= endRow; i++)
+		for (int j = startCol; j <= endCol; j++)
+			if (a[i][j] != temp)
+				return -2;
+	return temp;
+}
+
+void solve(int startRow, int endRow, int startCol, int endCol)
+{
+	int sameNum = checkSameNum(startRow, endRow, startCol, endCol);
+	if (sameNum == -2) {
+		int length = (endRow - startRow + 1) / 3;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				solve(startRow + i * length, startRow + i * length + length - 1, startCol + j * length, startCol + j * length + length - 1);
+			}
+		}
+	}
+	else
+		result[sameNum + 1]++;
 }
