17411f01249c0a2bb76c5b9be0319ee972ce43f1
Jason Jung <hyeonsuns@naver.com>
2018-03-30 16:20:46 +0900

    Daily coding divide and conquer_2

--- Divide_Conquer/Divide_Conquer/10816_숫자카드2.cpp
+++ Divide_Conquer/Divide_Conquer/10816_숫자카드2.cpp
@@ -1,44 +1,27 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <iostream>
+#include <set>
 using namespace std;
-
-int binary_search(vector<int> a, int num) {
-	int l = 0, r = a.size();
-	int mid;
-	while (l <= r) {
-		mid = (l + r) / 2;
-		if (a[mid] == num)
-			return 1;
-		else if (a[mid] < num)
-			r = mid - 1;
-		else
-			r = mid + 1;
-	}
-	return 0;
-}
-
-int main(void)
-{
+int main() {
 	int n;
 	scanf("%d", &n);
-	vector<int> a(n);
 
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
+	multiset<int> s;
+	for (int i = 0; i<n; i++) {
+		int x;
+		scanf("%d", &x);
+		s.insert(x);
+	}
+
 	int m;
 	scanf("%d", &m);
-	vector<int> b(m);
-	vector<int> count((*max_element(b.begin(), b.end())) + 1);
-	for (int i = 0; i < m; i++)
-		scanf("%d", &b[i]);
 
-	sort(a.begin(), a.end());
-	for (int i = 0; i < b.size(); i++)
-		count[b[i]] += binary_search(a, b[i]);
+	for (int i = 0; i<m; i++) {
+		int x;
+		scanf("%d", &x);
+		printf("%d ", s.count(x));
+	}
 
-	for (int i = 0; i < count.size(); i++)
-		printf("%d %d\n", b[i], count[i]);
+	printf("\n");
 
 	return 0;
 }
