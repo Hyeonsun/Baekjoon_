a8db691f7b1afbc2201d14ebd65541e9422059ae
Jason Jung <hyeonsuns@naver.com>
2018-04-03 18:20:49 +0900

    Daily coding 0403 divide and conquer

--- Divide_Conquer/Divide_Conquer/1992_쿼드트리.cpp
+++ Divide_Conquer/Divide_Conquer/1992_쿼드트리.cpp
@@ -4,16 +4,35 @@ using namespace std;
 
 int a[65][65];
 
+/*void solve(int rowStart, int rowEnd, int colStart, int colEnd) {
+int data = a[rowStart][colStart];
+
+
+
+for (int i = rowStart; i <= rowEnd; i++) {
+for (int j = colStart; j <= colEnd; j++) {
+if (data != a[i][j]) {
+printf("(");
+solve(rowStart, (rowEnd-1) / 2, colStart, (colEnd-1) / 2);
+solve((rowEnd + 1) / 2, rowEnd, colStart, (colEnd-1) / 2);
+solve(rowStart, (rowEnd-1) / 2, (colEnd + 1) / 2, colEnd);
+solve((rowEnd + 1) / 2, rowEnd, (colEnd + 1) / 2, colEnd);
+printf(")");
+}
+}
+}
+printf("%d %d %d %d -- %d\n", rowStart, rowEnd, colStart, colEnd, data);
+//printf("%d", data);
+return;
+}*/
 bool same(int x, int y, int n) {
-	for (int i = x; i<x + n; i++) {
-		for (int j = y; j<y + n; j++) {
-			if (a[x][y] != a[i][j]) {
+	for (int i = x; i<x + n; i++)
+		for (int j = y; j<y + n; j++)
+			if (a[x][y] != a[i][j])
 				return false;
-			}
-		}
-	}
 	return true;
 }
+
 void solve(int x, int y, int n) {
 	if (same(x, y, n)) {
 		printf("%d", a[x][y]);
@@ -21,23 +40,22 @@ void solve(int x, int y, int n) {
 	else {
 		printf("(");
 		int m = n / 2;
-		for (int i = 0; i<2; i++) {
-			for (int j = 0; j<2; j++) {
+		for (int i = 0; i<2; i++)
+			for (int j = 0; j<2; j++)
 				solve(x + m*i, y + m*j, m);
-			}
-		}
 		printf(")");
 	}
 }
-int main() {
+int main(void)
+{
 	int n;
 	scanf("%d", &n);
-	for (int i = 0; i<n; i++) {
-		for (int j = 0; j<n; j++) {
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
 			scanf("%1d", &a[i][j]);
-		}
-	}
+
 	solve(0, 0, n);
-	printf("\n");
+
 	return 0;
 }
