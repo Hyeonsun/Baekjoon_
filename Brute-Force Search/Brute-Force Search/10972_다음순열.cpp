ee060579bca6aa1bc654b971842542416345a4c4
Jason Jung <hyeonsuns@naver.com>
2018-04-08 21:32:51 +0900

    Daily coding Brute-force search

---
+++ Brute-Force Search/Brute-Force Search/10972_다음순열.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int a[10001];
+
+bool next_permutation(int n, int a[]) {
+	int i = n - 1;
+	while (i > 0 && a[i - 1] >= a[i]) i--;
+	if (i <= 0) return false;
+	int j = n - 1;
+	while (a[j] <= a[i - 1]) j--;
+	swap(a[i - 1], a[j]);
+	j = n - 1;
+	while (i < j) {
+		swap(a[i], a[j]);
+		i++; j--;
+	}
+	return true;
+}
+
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+	if (next_permutation(n, a)) {
+		for (int i = 0; i < n; i++)
+			printf("%d ", a[i]);
+		puts("");
+	}
+	else
+		printf("-1\n");
+
+	return 0;
+}
---
+++ Brute-Force Search/Brute-Force Search/10974_모든순열.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int a[10];
+
+bool next_permutation(int n, int a[]) {
+	int i = n - 1;
+	while (i > 0 && a[i - 1] >= a[i]) i--;
+	if (i <= 0) return false;
+	int j = n - 1;
+	while (a[j] <= a[i - 1]) j--;
+	swap(a[i - 1], a[j]);
+	j = n - 1;
+	while (i < j) {
+		swap(a[i], a[j]);
+		i++; j--;
+	}
+	return true;
+}
+
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+		a[i] = i + 1;
+
+	do {
+		for (int i = 0; i < n; i++)
+			printf("%d ", a[i]);
+		puts("");
+	} while (next_permutation(a, a + n));
+	return 0;
+}
