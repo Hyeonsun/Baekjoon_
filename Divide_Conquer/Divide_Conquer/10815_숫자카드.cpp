6cf1e65e97fcfd2aa6a3460e391837de5e8d6d71
Jason Jung <hyeonsuns123@gmail.com>
2018-05-09 14:01:54 +0900

    Daily Coding Divide And Conquer

--- Divide_Conquer/Divide_Conquer/10815_숫자카드.cpp
+++ Divide_Conquer/Divide_Conquer/10815_숫자카드.cpp
@@ -4,31 +4,50 @@
 using namespace std;
 int n, m;
 int a[500000];
+
 int binary_search(int num) {
 	int l = 0;
 	int r = n - 1;
 	while (l <= r) {
 		int mid = (l + r) / 2;
-		if (a[mid] == num)
-			return 1;
-		else if (a[mid] > num)
-			r = mid - 1;
+		if (a[mid] == num) {
+			return 1; // 찾았다!
+		}
+		else if (a[mid] > num) {
+			r = mid - 1; // 찾는 값이 중간값보다 작으면
+		}
 		else
-			l = mid + 1;
+			l = mid + 1;  // 찾는 값이 중간값보다 크면
 	}
 	return 0;
 }
-int main() {
+
+int main(void)
+{
+	/*scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+	scanf("%d", &a[i]);
+	}
+	sort(a, a + n);
+	scanf("%d", &m);
+	while (m--) {
+	int num;
+	scanf("%d", &num);
+	printf("%d", binary_search(num));
+	}
+	printf("\n");
+	return 0;*/
+
 	scanf("%d", &n);
-	for (int i = 0; i<n; i++)
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
+	}
 	sort(a, a + n);
 	scanf("%d", &m);
 	while (m--) {
 		int num;
 		scanf("%d", &num);
-		printf("%d ", binary_search(num));
+		printf("%d ", binary_search(a, a + n, num));
 	}
-	printf("\n");
-	return 0;
+	puts("");
 }
