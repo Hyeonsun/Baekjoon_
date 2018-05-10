a1905810920b55383729bb121a855cf384aafde7
Jason Jung <hyeonsuns123@gmail.com>
2018-05-10 11:08:50 +0900

    Daily coding 0509

--- Divide_Conquer/Divide_Conquer/10816_숫자카드2.cpp
+++ Divide_Conquer/Divide_Conquer/10816_숫자카드2.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
-#include <set>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
 using namespace std;
-int main() {
+
+int main(void)
+{
 	int n;
 	scanf("%d", &n);
+	vector<int> a(n);
 
-	multiset<int> s;
-	for (int i = 0; i<n; i++) {
-		int x;
-		scanf("%d", &x);
-		s.insert(x);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
 	}
+	sort(a.begin(), a.end());
 
 	int m;
 	scanf("%d", &m);
 
-	for (int i = 0; i<m; i++) {
-		int x;
-		scanf("%d", &x);
-		printf("%d ", s.count(x));
+	for (int i = 0; i < m; i++) {
+		int num;
+		scanf("%d", &num);
+		auto p = equal_range(a.begin(), a.end(), num);	// auto : 변수 자동 지정
+		auto l = lower_bound(a.begin(), a.end(), num);
+		auto r = lower_bound(a.begin(), a.end(), num);
+		printf("%d ", p.second - p.first);
+		printf("%d", r - l);
 	}
 
-	printf("\n");
-
+	puts("");
 	return 0;
 }
