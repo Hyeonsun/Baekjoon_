02fca7cb12b189bab277834de2bf82f35bb42e32
Jason Jung <hyeonsuns123@gmail.com>
2018-05-11 16:30:45 +0900

    Daily coding 0511

--- Divide_Conquer/Divide_Conquer/11729_하노이의탑_이동순서.cpp
+++ Divide_Conquer/Divide_Conquer/11729_하노이의탑_이동순서.cpp
@@ -1,31 +1,19 @@
 #include <cstdio>
-#include <vector>
 using namespace std;
 
-vector<int> From, To;
+void solve(int n, int x, int y) {
+	if (n == 0) return;
+	solve(n - 1, x, 6 - x - y);
+	printf("%d %d\n", x, y);
+	solve(n - 1, 6 - x - y, y);
+}
 
-void move(int from, int to) {
-	From.push_back(from);
-	To.push_back(to);
-}
-void hanoi(int n, int from, int by, int to) {
-	if (n == 1) {
-		move(from, to);
-		return;
-	}
-	hanoi(n - 1, from, to, by);
-	move(from, to);
-	hanoi(n - 1, by, from, to);
-}
 int main(void)
 {
-	int num;
-	scanf("%d", &num);
-
-	hanoi(num, 1, 2, 3);
-	printf("%d\n", From.size());
-	for (int i = 0; i < From.size(); i++)
-		printf("%d %d\n", From[i], To[i]);
+	int n;
+	scanf("%d", &n);
+	printf("%d\n", (1 << n) - 1);
+	solve(n, 1, 3);
 
 	return 0;
 }
