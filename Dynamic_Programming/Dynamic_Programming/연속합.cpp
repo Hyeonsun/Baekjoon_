9e12af634384f1e28ae6d16469e0c1533026a3c3
Jason Jung <hyeonsuns123@gmail.com>
2018-07-09 17:52:47 +0900

    Today I studied 0709

--- Dynamic_Programming/Dynamic_Programming/연속합.cpp
+++ Dynamic_Programming/Dynamic_Programming/연속합.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
 
+int d[100001], a[100001];
 int main(void)
 {
 	int n;
-	cin >> n;
-
-	vector<int> a(n + 1);
-	vector<int> d(n + 1);
+	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++)
-		cin >> a[i];
+		scanf("%d", &a[i]);
 
-	d[1] = a[1];
-	for (int i = 2; i <= n; i++)
+	d[0] = 0;
+	for (int i = 1; i <= n; i++) {
 		d[i] = max(a[i], d[i - 1] + a[i]);
+	}
 
-	long long ans = d[1];
+	int ans = d[1];
 	for (int i = 2; i <= n; i++)
 		if (ans < d[i])
 			ans = d[i];
 
-	cout << ans << '\n';
+	printf("%d\n", ans);
 
 	return 0;
 }
