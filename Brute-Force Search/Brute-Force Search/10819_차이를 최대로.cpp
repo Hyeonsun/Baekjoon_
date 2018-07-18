98a2a57cd5a0ca3d532678ed0124e846e3ff3033
Jason Jung <hyeonsuns123@gmail.com>
2018-07-18 18:59:27 +0900

    Today I studied BruteForce 0718

--- Brute-Force Search/Brute-Force Search/10819_차이를 최대로.cpp
+++ Brute-Force Search/Brute-Force Search/10819_차이를 최대로.cpp
@@ -1,22 +1,24 @@
+#include <cstdio>
 #include <algorithm>
-#include <iostream>
 #include <vector>
 using namespace std;
+
 int calculate(vector<int> &a) {
 	int sum = 0;
-	for (int i = 1; i<a.size(); i++) {
+	for (int i = 1; i < a.size(); i++) {
 		sum += abs(a[i] - a[i - 1]);
 	}
 	return sum;
 }
-int main() {
+
+int main(void)
+{
 	int n;
-	cin >> n;
+	scanf("%d", &n);
 
 	vector<int> a(n);
-	for (int i = 0; i<n; i++) {
-		cin >> a[i];
-	}
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
 
 	sort(a.begin(), a.end());
 
@@ -27,6 +29,7 @@ int main() {
 		ans = max(ans, temp);
 	} while (next_permutation(a.begin(), a.end()));
 
-	cout << ans << '\n';
+	printf("%d\n", ans);
+
 	return 0;
 }
