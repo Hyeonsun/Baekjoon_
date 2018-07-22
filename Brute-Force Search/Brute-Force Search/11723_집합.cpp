ce25bbcca38eea65d9c98697264e555bbb476001
Jason Jung <hyeonsuns123@gmail.com>
2018-07-22 17:39:42 +0900

    Today I studied BruteForce Bitmask 0722

--- Brute-Force Search/Brute-Force Search/11723_집합.cpp
+++ Brute-Force Search/Brute-Force Search/11723_집합.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 char b[111];
+
 int main(void)
 {
 	int n = 20;
 	int m;
 	scanf("%d", &m);
+
 	int s = 0;
 	int x;
 	while (m--) {
@@ -18,24 +20,24 @@ int main(void)
 		}
 		else if (!strcmp(b, "remove")) {
 			scanf("%d", &x); x--;
-			s = (s&~(1 << x));
+			s = (s & ~(1 << x));
 		}
 		else if (!strcmp(b, "check")) {
 			scanf("%d", &x); x--;
 			int res = (s&(1 << x));
-			if (res)
-				puts("1");
-			else
-				puts("0");
+			if (res) puts("1");
+			else puts("0");
 		}
 		else if (!strcmp(b, "toggle")) {
 			scanf("%d", &x); x--;
 			s = (s ^ (1 << x));
 		}
-		else if (!strcmp(b, "all"))
+		else if (!strcmp(b, "all")) {
 			s = (1 << n) - 1;
-		else if (!strcmp(b, "empty"))
+		}
+		else if (!strcmp(b, "empty")) {
 			s = 0;
+		}
 	}
 	return 0;
 }
