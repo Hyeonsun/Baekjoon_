b19b6fb31db6945b8d7dbd634dcdacf24132dc0e
Jasonarea <hyeonsuns@naver.com>
2018-07-30 17:18:51 +0900

    Baekjoon MacBook Setup

---
+++ Practice/Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by 정현선 on 2018. 7. 30..
+//  Copyright © 2018년 정현선. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
