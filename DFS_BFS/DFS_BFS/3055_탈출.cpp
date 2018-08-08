9f8247d329db89ef565e48c3c0433e22e8678bd2
Jasonarea <hyeonsuns@naver.com>
2018-08-09 02:00:57 +0900

    Today I studied BFS Advanced Finish 0808

--- DFS_BFS/DFS_BFS/3055_탈출.cpp
+++ DFS_BFS/DFS_BFS/3055_탈출.cpp
@@ -1,77 +1,88 @@
 #include <iostream>
-#include <tuple>
-#include <algorithm>
-#include <cstring>
-#include <queue>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <deque>
+#include <tuple>
 using namespace std;
-int water[50][50];
-int d[50][50];
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<string> a(n);
-    for (int i=0; i<n; i++) {
-        cin >> a[i];
-    }
-    memset(water,-1,sizeof(water));
-    queue<pair<int,int>> q;
-    int sx,sy,ex,ey;
+vector<vector<int>> bfs(vector<string> &a, int x, int y) {
+    int n = a.size();
+    int m = a[0].size();
+    vector<vector<int>> d(n, vector<int>(m));
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
-            if (a[i][j] == '*') {
-                q.push(make_pair(i,j));
-                water[i][j] = 0;
-            } else if (a[i][j] == 'S') {
-                sx = i;
-                sy = j;
-            } else if (a[i][j] == 'D') {
-                ex = i;
-                ey = j;
-            }
+            d[i][j] = -1;
         }
     }
+    deque<pair<int,int>> q;
+    q.push_back(make_pair(x, y));
+    d[x][y] = 0;
     while (!q.empty()) {
-        int x, y;
-        tie(x,y) = q.front(); q.pop();
+        tie(x,y) = q.front(); q.pop_front();
         for (int k=0; k<4; k++) {
             int nx = x+dx[k];
             int ny = y+dy[k];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                continue;
-            }
-            if (water[nx][ny] != -1) continue;
-            if (a[nx][ny] == 'X') continue;
-            if (a[nx][ny] == 'D') continue;
-            water[nx][ny] = water[x][y] + 1;
-            q.push(make_pair(nx,ny));
-        }
-    }
-    memset(d,-1,sizeof(d));
-    q.push(make_pair(sx,sy));
-    d[sx][sy] = 0;
-    while (!q.empty()) {
-        int x, y;
-        tie(x,y) = q.front(); q.pop();
-        for (int k=0; k<4; k++) {
-            int nx = x+dx[k];
-            int ny = y+dy[k];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                continue;
-            }
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
             if (d[nx][ny] != -1) continue;
-            if (a[nx][ny] == 'X') continue;
-            if (water[nx][ny] != -1 && d[x][y]+1 >= water[nx][ny]) continue;
-            d[nx][ny] = d[x][y] + 1;
-            q.push(make_pair(nx,ny));
+            if (a[nx][ny] == '*') continue;
+            if (a[nx][ny] == '#') {
+                d[nx][ny] = d[x][y] + 1;
+                q.push_back(make_pair(nx,ny));
+            } else {
+                d[nx][ny] = d[x][y];
+                q.push_front(make_pair(nx,ny));
+            }
         }
     }
-    if (d[ex][ey] == -1) {
-        cout << "KAKTUS\n";
-    } else {
-        cout << d[ex][ey] << '\n';
+    return d;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<string> a(n+2);
+        for (int i=1; i<=n; i++) {
+            cin >> a[i];
+            a[i] = "." + a[i] + ".";
+        }
+        n += 2;
+        m += 2;
+        for (int j=0; j<m; j++) {
+            a[0] += ".";
+            a[n-1] += ".";
+        }
+        vector<vector<int>> d0 = bfs(a, 0, 0);
+        int x1, y1, x2, y2;
+        x1 = y1 = x2 = y2 = -1;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (a[i][j] == '$') {
+                    if (x1 == -1) {
+                        x1 = i;
+                        y1 = j;
+                    } else {
+                        x2 = i;
+                        y2 = j;
+                    }
+                }
+            }
+        }
+        vector<vector<int>> d1 = bfs(a, x1, y1);
+        vector<vector<int>> d2 = bfs(a, x2, y2);
+        int ans = n*m;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (a[i][j] == '*') continue;
+                int cur = d0[i][j] + d1[i][j] + d2[i][j];
+                if (a[i][j] == '#') cur -= 2;
+                if (ans > cur) ans = cur;
+            }
+        }
+        cout << ans << '\n';
     }
     return 0;
 }
